cf20dd6d77bc92b41f36e09067d69c72b7f2e1dc
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-12-19 09:35:12 +0100

    added progress-bar helper

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -12,12 +12,51 @@ function yell(msg) {
 function section(name, options) {
 
   if (!this._sections) this._sections = {};
+  if (!this._sections.head) {
+    this._sections[name] = options.fn(this);
+  } else {
+    this._sections[name] += options.fn(this);
+  }
+  return null;
 
-  this._sections[name] = options.fn(this);
+}
+
+function progress(current, max, id) {
+
+  if (!this._sections) this._sections = {};
+  if (!this._sections.head) {
+    this._sections.head = '<link rel="stylesheet" href="/css/progress_bars.css">';
+  } else {
+    this._sections.head += '<link rel="stylesheet" href="/css/progress_bars.css">';
+  }
+  console.log(typeof id);
+  out = '<div class="progress-bar-wrapper"'+
+        (typeof id === 'string' ? ` id="${id}">` : '>');
+  let cbar = '<div class="bar compleated"></div>';
+  let bar = '<div class="bar"></div>';
+  let cstage = '<div class="stage compleated"><div></div></div>';
+  let custage = '<div class="stage current"><div></div></div>';
+  let stage = '<div class="stage"><div></div></div>';
+
+  for (let i = 0; i < max; i++) {
+    if (i != 0) out += i < current ? cbar : bar;
+    if (i < current-1) {
+      out += cstage;
+    } else if (i == current-1) {
+      out += custage;
+    } else {
+      out += stage;
+    }
+  }
+
+  out += '</div>';
+
+  return out;
 
 }
 
 module.exports = {
   yell,
-  section
+  section,
+  progress
 };
