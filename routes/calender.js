51636a51e0571722501bfccd9918e05947aa434d
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-12-20 10:29:44 +0100

    calender v2

--- routes/calender.js
+++ routes/calender.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('calender');
+  res.render('calendar_v2');
 });
 
 module.exports = router;
