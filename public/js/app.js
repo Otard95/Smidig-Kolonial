0182997b699950caa124da7cd0bd6c1a82ce43fe
Unknown <parjoona@hotmail.com>
2017-12-20 15:18:59 +0100

    Fix animation, added partial

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,20 @@
 function start() {
-  let weekbtn, weeklist;
   let boolean = true;
 
-  const init = function() {
-    const elements = function() {
+  $(".week-add-button").click(function() {
+    if (boolean) {
+      $(".week-add-button").animate({
+        bottom: '29em'
+      }, 1000);
+      boolean = false;
+    } else {
+      $(".week-add-button").animate({
+        bottom: 0
+      }, 1000);
+      boolean = true;
+    }
+  });
 
-      if (window.location.pathname === '/uke') {
-        weekbtn = document.querySelectorAll('.week-add-button')[0];
-        weeklist = document.querySelectorAll('.week-view-list')[0];
-      }
-    }();
-    const events = function() {
-      weekbtn.onclick = () => {
-        if (boolean) {
-          weeklist.style.cssText = `height: 0em`;
-          boolean = false;
-        } else {
-          weeklist.style.cssText = `height: 35em`;
-          boolean = true;
-        }
-      }
-    }();
-  }();
 }
 
 $(document).ready(start);
