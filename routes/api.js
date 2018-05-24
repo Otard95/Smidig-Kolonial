e071f76132c9d0624769b95a3a0e084b8e282b79
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-05-24 11:56:07 +0200

    routes for kolonial-api-wrapper

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,12 @@ router.get('/', (req, res) => {
 		routes: [
 			'/',
 			'/categories',
-			'/category/:id'
+			'/category/:id',
+			'/item/:id',
+			'/item/search?name=<item name>',
+			'/recipe/search?name=<name query>',
+			'/recipe/suggested',
+			'/recipe/:id'
 		]
 	})
 
@@ -23,31 +28,158 @@ router.get('/', (req, res) => {
 
 router.get('/categories', async function (req, res, next) {
 
-	let categories = await api.GetAllCategories();
+	try {
 
-	res.json({
-		categories
-	});
+		let categories = await api.GetAllCategories();
+		res.json(categories);
 
+	} catch (e) {
+
+		res.status(500);
+		res.json({ error: e });
+
+	}
+	
 });
 
 router.get('/category/:id', async (req, res) => {
 
-	let int = parseInt(req.params.id);
-	if (int !== NaN && ('' + int) === req.params.id) {
-
-		try {
-			let api_res = await api.GetCategoryById(int);
-			res.json(api_res);
-		} catch (e) {
-			res.status(404);
-			res.json(e);
-		}
-
-	} else {
+	let id = parseInt(req.params.id);
+	if (id === NaN || ('' + id) !== req.params.id) {
 
 		res.status(404);
 		res.json({ message: 'Can only get category by id.' });
+		return;
+
+	}
+
+	try {
+
+		let api_res = await api.GetCategoryById(id);
+		res.json(api_res);
+
+	} catch (e) {
+
+		res.status(404);
+		res.json(e);
+
+	}
+
+});
+
+router.get('/item/search', async (req, res) => {
+
+	let item_name = req.query.name;
+
+	if (typeof item_name !== 'string') {
+
+		res.status(400);
+		res.json({ message: 'Query string "name" required.' });
+		return;
+
+	}
+
+	try {
+
+		let items = await api.GetItemBySearch(item_name);
+		res.json(items);
+
+	} catch (e) {
+
+		res.status(404);
+		res.json(e);
+
+	}
+
+});
+
+router.get('/item/:id', async (req, res) => {
+
+	let id = parseInt(req.params.id);
+	if (id === NaN || ('' + id) !== req.params.id) {
+
+		res.status(404);
+		res.json({ message: 'Invalid id format or resouce.' });
+		return;
+
+	}
+
+	try {
+
+		let api_res = await api.GetItemById(id);
+		res.json(api_res);
+
+	} catch (e) {
+
+		res.status(404);
+		res.json(e);
+
+	}
+
+});
+
+router.get('/recipe/search', async (req, res) => {
+
+	let recipe_name = req.query.name;
+
+	if (typeof recipe_name !== 'string') {
+
+		res.status(400);
+		res.json({ message: 'Query string "name" required.' });
+		return;
+
+	}
+
+	try {
+
+		let items = await api.GetRecipeBySearch(recipe_name);
+		res.json(items);
+
+	} catch (e) {
+
+		res.status(404);
+		res.json(e);
+
+	}
+
+});
+
+router.get('/recipe/suggested', async (req, res) => {
+
+	try {
+
+		let items = await api.GetRecipeSuggestions();
+		res.json(items);
+
+	} catch (e) {
+
+		res.status(404);
+		res.json(e);
+
+	}
+
+});
+
+router.get('/recipe/:id', async (req, res) => {
+
+	let id = parseInt(req.params.id);
+	if (id === NaN || ('' + id) !== req.params.id) {
+
+		res.status(404);
+		res.json({ message: 'Invalid id format or resource.' });
+		return;
+
+	}
+
+	try {
+
+		let api_res = await api.GetRecipeById(id);
+		res.json(api_res);
+
+	} catch (e) {
+
+		res.status(404);
+		res.json(e);
 
 	}
 
