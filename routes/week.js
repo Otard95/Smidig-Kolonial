221db1dbb28fa7ce4604c34eea6dcb361fb25777
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-05-29 11:02:54 +0200

    week day fix

--- routes/week.js
+++ routes/week.js
@@ -4,12 +4,19 @@ const router = require('express').Router();
 /* GET home page. */
 router.get('/:mon-:day', (req, res, next) => {
 
-  let mon = req.params.mon
-  let day = req.params.day
+  let mon = parseInt(req.params.mon);
+  let day = parseInt(req.params.day);
+
+  if (mon === NaN || '' + mon !== req.params.mon ||
+      day === NaN || '' + day !== req.params.day)
+  {
+    next({
+      msg: 'parameter error'
+    });
+    return;
+  }
 
   // Required  to pass month down to render
-  let chosen_day = day
-  let month = new Date(2018, mon, day).getMonth()
   let months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember']
 
   function getWeekNumber(month, day) {
@@ -21,35 +28,54 @@ router.get('/:mon-:day', (req, res, next) => {
 
   function getNumbersInWeek(year, month, daynum) {
     let arr = []
-    let day = new Date(year, month, daynum).getDay()
-    // date = new Date(year, month - 1, daynum);
-    while (arr.length < 7) {
-      let date = new Date(year, month, daynum)
-      // Difference checks if first days in week is negative (in to last month)
-      let differance = date.getDate() - day + (day == 0 ? -6 : arr.length + 1)
-      // If diff is < 1, starts from month before
-      if (differance < 1) {
-        arr.push(new Date(year, month - 1, differance).getDate())
-      } else {
-        let monthlength = new Date(year, month, 0).getDate() + 1
-        let thisdate = date.getDate() - day + (day == 0 ? -6 : arr.length + 1)
-        // checks if date is over the month length
-        thisdate > monthlength ? arr.push(thisdate - monthlength) : arr.push(thisdate)
-      }
+    let date = new Date(year, month, daynum);
+    let week_day = date.getDay() - 1;
+
+    // get the date og the first day of the week
+    let first_date = date.getDate() - (week_day == -1 ? 6 : week_day);
+    let mon_len = new Date(year, month + 1, 0).getDate();
+
+    if (first_date < 1) {
+      // Lengthog the month the week start in
+      mon_len = new Date(year, month, 0).getDate();
+      first_date = mon_len + first_date;
     }
+
+    for (let i = 0; i < 7; i++) {
+      let temp = (first_date + i) % (mon_len + 1);
+      temp += (temp < first_date ? 1 : 0);
+      arr.push(temp);
+    }
+
+    // // date = new Date(year, month - 1, daynum);
+    // while (arr.length < 7) {
+    //   let date = new Date(year, month, daynum)
+    //   // Difference checks if first days in week is negative (in to last month)
+    //   let differance = date.getDate() - day + (day == 0 ? -6 : arr.length + 1)
+    //   // If diff is < 1, starts from month before
+    //   if (differance < 1) {
+    //     arr.push(new Date(year, month - 1, differance).getDate())
+    //   } else {
+    //     let monthlength = new Date(year, month, 0).getDate() + 1
+    //     let thisdate = date.getDate() - day + (day == 0 ? -6 : arr.length + 1)
+    //     // checks if date is over the month length
+    //     thisdate > monthlength ? arr.push(thisdate - monthlength) : arr.push(thisdate)
+    //   }
+    // }
+
     return arr
   }
 
-  let uke_num = getWeekNumber(mon, day)
-  let days_arr = getNumbersInWeek(2018, mon, day)
+  let uke_num = getWeekNumber(mon-1, day)
+  let days_arr = getNumbersInWeek(2018, mon-1, day)
 
   res.render('week', {
-    title: `Calendar: ${day} ${months[month]}`,
-    month,
+    title: `Calendar: ${day} ${months[mon]}`,
+    month: mon,
     uke_num,
     daysstring: ['Man', 'Tis', 'Ons', 'Tor', 'Fre', 'Lor', 'Son'],
     days_arr,
-    chosen_day
+    chosen_day: day
   });
 });
 
