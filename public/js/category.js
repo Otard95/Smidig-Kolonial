ff1fd3440defddd9c2a1c3b2623a57cb12cbf5dd
Unknown <parjoona@hotmail.com>
2018-05-31 22:05:14 +0200

    Fixed undefined issue (one-line)

--- public/js/category.js
+++ public/js/category.js
@@ -1,47 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-let btn = document.querySelector(".icon-button");
-let categoryBox = document.querySelector(".category-container");
-let changeButton = document.querySelector(".icon-button");
-let whiteIconGone = document.querySelector(".add-product-button");
-let blackIconShow = document.querySelector(".button-white");
-let catlist = document.querySelector(".category-list")
+  let btn = document.querySelector(".icon-button");
+  let categoryBox = document.querySelector(".category-container");
+  let changeButton = document.querySelector(".icon-button");
+  let whiteIconGone = document.querySelector(".add-product-button");
+  let blackIconShow = document.querySelector(".button-white");
+  let catlist = document.querySelector(".category-list")
 
-btn.addEventListener("click", () => {
+  btn.addEventListener("click", () => {
 
-  changeButton.classList.toggle("change-button");
-  categoryBox.classList.toggle("show");
-  whiteIconGone.classList.toggle("hide-button");
-  blackIconShow.classList.toggle("show-button");
+    changeButton.classList.toggle("change-button");
+    categoryBox.classList.toggle("show");
+    whiteIconGone.classList.toggle("hide-button");
+    blackIconShow.classList.toggle("show-button");
 
-})
+  })
 
-function setEventListener() {
-  let listitem = document.querySelectorAll(".list-item")
-  listitem.forEach((i) => {
-    i.addEventListener("click", () => {
-      let cat = i.dataset.categoryid
-      fetch(`/API/category/${cat}`)
-        .then(data => data.json())
-        .then(d => {
-          console.log(d);
-          if(d.children.length > 0){
-            catlist.innerHTML = ""
-            d.children.forEach((j) => {
-              fetch(`/API/category/${j}`)
-                .then(dat => dat.json())
-                .then(k => {
-                  catlist.innerHTML += `<li class="list-item" data-categoryid="${k.id}">${k.name}</li>`
-                })
-            })
-          }else{
-            catlist.innerHTML = ""
-            d.products.forEach((j) => {
-              fetch(`/API/item/${j}`)
-                .then(dat => dat.json())
-                .then(k => {
-                  console.log(k);
-                  catlist.innerHTML += `<div class="product-container">
+  function setEventListener() {
+    let listitem = document.querySelectorAll(".list-item")
+    listitem.forEach((i, index) => {
+      i.addEventListener("click", () => {
+        let cat = i.dataset.categoryid
+        fetch(`/API/category/${cat}`)
+          .then(data => data.json())
+          .then(y => {
+
+            if (y.children.length > 0) {
+              catlist.innerHTML = ""
+              y.children.forEach((j) => {
+                fetch(`/API/category/${j}`)
+                  .then(x => x.json())
+                  .then(k => {
+                    if (k.name !== undefined) catlist.innerHTML += `<li class="list-item" data-categoryid="${k.id}">${k.name}</li>`
+                  })
+              })
+
+            } else {
+              catlist.innerHTML = ""
+              y.products.forEach((j) => {
+                fetch(`/API/item/${j}`)
+                  .then(dat => dat.json())
+                  .then(k => {
+                    catlist.innerHTML += `<div class="product-container">
                   <img id="product-image" src=${k.images[0].thumbnail.url} alt="">
                   <h1 id="product-name">${k.name.base}</h1>
                   <h1 id="price-per-unit">kr ${k.price.gross}</h1>
@@ -49,17 +49,16 @@ function setEventListener() {
                   <h2 id="price-som">${k.price.unit_quantity_name}</h2>
                   <h2 id="price-quantity">kr ${k.price.gross_unit}/${k.price.unit_quantity_abbreviation}</h2>
                   </div>`
-                })
-            })
-          }
-
-        })
+                  })
+              })
+            }
+          })
         setTimeout(() => {
-        setEventListener()
-      }, 1500)
+          setEventListener()
+        }, 1000)
+      })
     })
-  })
-}
+  }
 
-setEventListener()
+  setEventListener()
 })
