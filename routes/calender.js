907fbc309a93a4c2a50c638cbe9befd64459318a
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-05-31 15:53:46 +0200

    untested get dates. need to merge

--- routes/calender.js
+++ routes/calender.js
@@ -5,6 +5,21 @@ const OAuth = require('../bin/OAuth');
 const week = require('./week.js');
 const shopping_list_service = require('../bin/shopping-list');
 
+async function GetShoppingListsDates (arr_list) {
+
+  prom = [];
+  arr_list.forEach( id => {
+    prom.push(shopping_list_service.getShoppingList(id));
+  });
+  let res = await Promise.all(prom);
+
+  res = res.filter(SLRes => ShoppingListResponse.OK(SLRes));
+  res = res.map(SLRes => SLRes.data.date);
+
+  return res;
+
+}
+
 router.use('/', OAuth.Authorized(url.format({ // /user/login?redirect=<redirect-uri>&m=<message>
   pathname: '/user/login',
   query: {
@@ -17,6 +32,10 @@ router.use('/', OAuth.Authorized(url.format({ // /user/login?redirect=<redirect-
 router
   .get('/:mon-:day', week) // Gets week 
   .get('/:mon?', function (req, res, next) {
+
+    let res = await GetShoppingListsDates(req.user.lists);
+    console.log(res);
+
     res.render('calendar', {
       title: 'Kalender',
       month: req.params.mon ? req.params.mon : new Date().getMonth()
