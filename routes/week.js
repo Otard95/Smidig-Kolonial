ccd87807cc0889903bf37b7fdc8b5f5fa058faaf
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-06-01 11:54:05 +0200

    moved functions outside route

--- routes/week.js
+++ routes/week.js
@@ -25,6 +25,36 @@ async function GetListOnDate (num_date, arr_list_ids) {
 
 }
 
+function getWeekNumber(month, day) {
+  date = new Date(Date.UTC(2018, month, day));
+  date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+  let yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+  return Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
+}
+
+function getNumbersInWeek(year, month, daynum) {
+  let arr = []
+  let date = new Date(year, month, daynum);
+  let week_day = date.getDay() - 1;
+
+  // get the date og the first day of the week
+  let first_date = date.getDate() - (week_day == -1 ? 6 : week_day);
+  let mon_len = new Date(year, month + 1, 0).getDate();
+
+  if (first_date < 1) {
+    // Lengthog the month the week start in
+    mon_len = new Date(year, month, 0).getDate();
+    first_date = mon_len + first_date;
+  }
+
+  for (let i = 0; i < 7; i++) {
+    let temp = (first_date + i) % (mon_len + 1);
+    temp += (temp < first_date ? 1 : 0);
+    arr.push(temp);
+  }
+  return arr
+}
+
 /* GET home page. */
 router.get('/:mon-:day', async (req, res, next) => {
   let mon = parseInt(req.params.mon);
@@ -46,36 +76,6 @@ router.get('/:mon-:day', async (req, res, next) => {
   let product = await api.GetItemById(520)
   let months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember']
 
-  function getWeekNumber(month, day) {
-    date = new Date(Date.UTC(2018, month, day));
-    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
-    let yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
-    return Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
-  }
-
-  function getNumbersInWeek(year, month, daynum) {
-    let arr = []
-    let date = new Date(year, month, daynum);
-    let week_day = date.getDay() - 1;
-
-    // get the date og the first day of the week
-    let first_date = date.getDate() - (week_day == -1 ? 6 : week_day);
-    let mon_len = new Date(year, month + 1, 0).getDate();
-
-    if (first_date < 1) {
-      // Lengthog the month the week start in
-      mon_len = new Date(year, month, 0).getDate();
-      first_date = mon_len + first_date;
-    }
-
-    for (let i = 0; i < 7; i++) {
-      let temp = (first_date + i) % (mon_len + 1);
-      temp += (temp < first_date ? 1 : 0);
-      arr.push(temp);
-    }
-    return arr
-  }
-
   let uke_num = getWeekNumber(mon - 1, day)
   let days_arr = getNumbersInWeek(2018, mon - 1, day)
 
@@ -87,7 +87,7 @@ router.get('/:mon-:day', async (req, res, next) => {
     days_arr,
     chosen_day: day,
     categories,
-    product,
+    product
   })
 })
 
