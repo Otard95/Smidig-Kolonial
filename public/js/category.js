c05ed6ad4c790564e9a397567621c0d4f0a3978b
Unknown <parjoona@hotmail.com>
2018-06-01 12:40:29 +0200

    Updated for new API changes

--- public/js/category.js
+++ public/js/category.js
@@ -18,8 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function getCategories(data) {
     catlist.innerHTML = ""
-    let list = []
-    data.children.forEach(async (num, index) => {
+    data.children_id.forEach(async (num, index) => {
       try {
         let response = await fetch(`/API/category/${num}`)
         let json = await response.json()
@@ -38,12 +37,12 @@ document.addEventListener("DOMContentLoaded", () => {
         let response = await fetch(`/API/item/${j}`)
         let json = await response.json()
         catlist.innerHTML += `<div class="product-container">
-                  <img id="product-image" src=${json.images[0].thumbnail.url} alt="">
-                  <h1 id="product-name">${json.name.base}</h1>
+                  <img id="product-image" src=${json.images.thumbnail} alt="">
+                  <h1 id="product-name">${json.name}</h1>
                   <h1 id="price-per-unit">kr ${json.price.gross}</h1>
                   <img id="add-button" src="/imgs/icon/Velg vare.png" alt="">
-                  <h2 id="price-som">${json.price.unit_quantity_name}</h2>
-                  <h2 id="price-quantity">kr ${json.price.gross_unit}/${json.price.unit_quantity_abbreviation}</h2>
+                  <h2 id="price-som">stykk</h2>
+                  <h2 id="price-quantity">kr ${json.price.gross_unit}</h2>
                   </div>`
         setEventListener()
       } catch (e) {
@@ -58,10 +57,11 @@ document.addEventListener("DOMContentLoaded", () => {
     listitem.forEach(item => {
       item.addEventListener("click", async () => {
         let category = item.dataset.categoryid
-        let data = await fetch(`/API/category/${category}`)
+        let data = await fetch(`/api/category/${category}`)
         let json = await data.json()
+        console.log(json)
 
-        if (json.children.length > 0) {
+        if (json.children_id && json.children_id.length > 0) {
           getCategories(json)
         } else {
           console.log('Should log out data')
