efeb4caabbc439190378afe2bd7f878aa49e0e4d
rikke59 <sveull16@student.westerdals.no>
2018-06-01 15:54:45 +0200

    Search function working

--- public/js/category.js
+++ public/js/category.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
     listitem.forEach(item => {
       item.addEventListener("click", async () => {
         let category = item.dataset.categoryid
-        let data = await fetch(`/api/category/${category}`)
+        let data = await fetch(`/API/category/${category}`)
         let json = await data.json()
 
         if (json.children_id && json.children_id.length > 0) {
@@ -79,6 +79,29 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     })
   }
+  function searchTerm(){
+    let input = document.getElementById("search-t")
+    input.addEventListener("change", async () => {
+      console.log(input.value)
+      let i = input.value
+      let data = await fetch(`/API/item/search?name=${i}`)
+      let json = await data.json()
+      console.log(json)
+      catlist.innerHTML = ""
 
+      json.forEach(async (json) => {
+          catlist.innerHTML += `<div class="product-container">
+                    <img id="product-image" src=${json.images.thumbnail} alt="">
+                    <h1 id="product-name">${json.name}</h1>
+                    <h1 id="price-per-unit">kr ${json.price.gross}</h1>
+                    <img id="add-button" src="/imgs/icon/Velg vare.png" alt="">
+                    <h2 id="price-som">stykk</h2>
+                    <h2 id="price-quantity">kr ${json.price.gross_unit}</h2>
+                    </div>`
+          setEventListener()
+        })
+    })
+  }
   setEventListener()
+  searchTerm()
 })
