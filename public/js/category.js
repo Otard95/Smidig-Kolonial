c8e84860680a04dc98277d608b3941119f253266
rikke59 <sveull16@student.westerdals.no>
2018-06-05 15:26:59 +0200

    started on static inspiration page

--- public/js/category.js
+++ public/js/category.js
@@ -6,25 +6,28 @@ document.addEventListener("DOMContentLoaded", () => {
   let blackIconShow = document.querySelector(".button-white");
   let catlist = document.querySelector(".category-list");
   let showArrow = document.querySelector(".back-arrow");
+  let inspoCategory = document.querySelector(".inspiration");
+  let headerName = document.querySelector(".search-title");
+  let headerContainer = document.querySelector(".information-header")
+  let hideSearch = document.querySelector(".search-container");
+  let inspoList = document.querySelector(".categories")
 
   let itemId = []
-
 
   btn.addEventListener("click", async () => {
     // Resets the list
     if (btn.classList.contains("change-button")) {
       let result = await fetch('/api/categories')
       let json = await result.json()
-
       catlist.innerHTML = ""
-      catlist.innerHTML += `<li>Inspirasjon</li>
+      catlist.innerHTML += `<li class="inspiration">Inspirasjon</li>
       <li>Dine Varer</li>`
       json.forEach(e => {
         catlist.innerHTML += `<li class="list-item" data-categoryid="${e.id}">${e.name}</li>`
         setEventListener()
       })
     }
-
+    inspirationClick()
     btn.classList.toggle("change-button");
     categoryBox.classList.toggle("show");
     whiteIconGone.classList.toggle("hide-button");
@@ -32,6 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 
   async function getCategories(data) {
+    headerName.innerHTML = ""
+    headerName.innerHTML = `${data.name}`
     catlist.innerHTML = ""
     data.children_id.forEach(async (num, index) => {
       try {
@@ -46,6 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   async function getProducts(data) {
+    headerName.innerHTML = ""
+    headerName.innerHTML = `${data.name}`
     catlist.innerHTML = ""
     data.products.forEach(async (j) => {
       try {
@@ -122,9 +129,12 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(json);
         getCategories(json)
       }if(itemId.length < 2){
+        hideSearch.classList.remove("hide-search-container")
+        inspoList.classList.remove("inspiration-categories")
         let result = await fetch('/api/categories')
         let json = await result.json()
-
+        headerName.innerHTML = ""
+        headerName.innerHTML = `Varer`
         catlist.innerHTML = ""
         catlist.innerHTML += `<li>Inspirasjon</li>
         <li>Dine Varer</li>`
@@ -134,6 +144,57 @@ document.addEventListener("DOMContentLoaded", () => {
         })
       }
       itemId.pop()
+    })
+  }
+
+  function inspirationClick(){
+    inspoCategory.addEventListener("click", () =>{
+      console.log("Inspo Clicked");
+      headerName.innerHTML = ""
+      headerName.innerHTML = `Inspirasjon`
+      catlist.innerHTML = ""
+      catlist.innerHTML += `<li>Familiemiddag</li>
+        <li>Helgekos</li>
+        <li class="event">Selskap</li>
+        <li>Gjester</li>
+        <li>Høytid</li>`
+      hideSearch.classList.toggle("hide-search-container")
+      inspoList.classList.toggle("inspiration-categories")
+
+      let eventCategory = document.querySelector(".event")
+      eventCategory.addEventListener("click", async () =>{
+        await console.log("Noe ble trykket på");
+        catlist.innerHTML = ""
+        catlist.innerHTML += `<li class="kidsBirthday">Barnebursdag</li>
+          <li>Fest</li>
+          <li>Jubeleum</li>`
+
+        let birthdayCategory = document.querySelector(".kidsBirthday")
+        birthdayCategory.addEventListener("click", async () =>{
+          await console.log("Barnebursdag klikket på");
+          headerContainer.classList.toggle("information-event-header")
+          headerName.innerHTML = ""
+          headerName.innerHTML = `Barnebursdag`
+          catlist.innerHTML = ""
+
+          let data = await fetch(`/API/item/search?name=bursdag`)
+          let json = await data.json()
+          console.log(json)
+          catlist.innerHTML = ""
+
+          json.forEach(async (json) => {
+              catlist.innerHTML += `<div class="product-container">
+                        <img id="product-image" src=${json.images.thumbnail} alt="">
+                        <h1 id="product-name">${json.name}</h1>
+                        <h1 id="price-per-unit">kr ${json.price.gross}</h1>
+                        <img id="add-button" src="/imgs/icon/Velg vare.png" alt="">
+                        <h2 id="price-som">stykk</h2>
+                        <h2 id="price-quantity">kr ${json.price.gross_unit}</h2>
+                        </div>`
+              setEventListener()
+            })
+        })
+      })
     })
   }
   setEventListener()
